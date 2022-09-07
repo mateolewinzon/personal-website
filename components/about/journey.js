8e64011a82227dc68e6419cd4384550f30ecbfd0
unknown <mateolewinzon@gmail.com>
2022-09-07 15:29:48 -0300

    issing key prop

--- components/about/journey.js
+++ components/about/journey.js
@@ -2,15 +2,15 @@ import { Container, Row } from "react-bootstrap";
 import text from "../../config/text";
 import { typewriter } from "../../styles/typing-effect.module.css";
 
-function Skills() {
+function Journey() {
   const { my_journey_paragraphs, my_journey} = text["en"];
 
   return (
     <Row className="pt-5 pb-5">
       <span className='semititle pb-4'>{my_journey}</span>
-        {my_journey_paragraphs.map((text)=> <p>{text}</p>)}
+        {my_journey_paragraphs.map((text, i)=> <p key={i}>{text}</p>)}
     </Row>
   );
 }
 
-export default Skills;
+export default Journey;
