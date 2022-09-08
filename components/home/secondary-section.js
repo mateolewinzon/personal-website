7ebb59ad2ac77b097dbdb5fd8f43e469bcd61578
unknown <mateolewinzon@gmail.com>
2022-09-08 12:19:59 -0300

    changed button position

--- components/home/secondary-section.js
+++ components/home/secondary-section.js
@@ -6,29 +6,22 @@ function SecondarySection() {
   const { recent_posts, learn_more } = text["es"];
 
   return (
-    <Row className="dark-section">
-      <Col xs="12" md="4" lg="5" className="text-center text-md-end  p-xl-4">
-        <Button className="btn" href={"about"}>
-          {learn_more}
-        </Button>
-      </Col>
-      <Col md="5" lg="6" className="p-xl-4">
+    <Row className="dark-section text-center">
+      <Col xs="auto">
         <Row>
-          <Row>
-            <div className="blogsTitle">{recent_posts}</div>
-          </Row>
-          <Row>
-            {Array(5)
-              .fill({
-                name: "This blog will load really fast. But why?",
-                date: "2022-03-09",
-              })
-              .map((post, key) => (
-                <span key={key} className="blogsItem">
-                  {post.date} {post.name}
-                </span>
-              ))}
-          </Row>
+          <div className="blogsTitle">{recent_posts}</div>
+        </Row>
+        <Row>
+          {Array(5)
+            .fill({
+              name: "This blog will load really fast. But why?",
+              date: "2022-03-09",
+            })
+            .map((post, key) => (
+              <span key={key} className="blogsItem">
+                {post.date} {post.name}
+              </span>
+            ))}
         </Row>
       </Col>
     </Row>
--- components/home/top-section.js
+++ components/home/top-section.js
@@ -1,9 +1,10 @@
 import { Container, Row } from "react-bootstrap";
 import text from "../../config/text";
 import { typewriter } from "../../styles/typing-effect.module.css";
+import Button from "../common/Button";
 
 function TopSection() {
-  const { subtitle, title, welcome_text } = text["es"];
+  const { subtitle, title, welcome_text, learn_more } = text["es"];
 
   return (
     <Row className="top-section">
@@ -16,8 +17,13 @@ function TopSection() {
       <Row className="pb-4">
         <hr className="hr" />
       </Row>
-      <Row>
+      <Row className="pb-4">
         <span className="secondaryText">{welcome_text}</span>
+      </Row>
+      <Row className="pb-4">
+        <Button className="btn" href={"about"}>
+          {learn_more}
+        </Button>
       </Row>
     </Row>
   );
