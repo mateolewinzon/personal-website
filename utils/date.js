63439a39f1a8489e09e847d2105e933a66102882
unknown <mateolewinzon@gmail.com>
2022-09-13 12:52:27 -0300

    fix date mac/ios

--- utils/date.js
+++ utils/date.js
@@ -1,10 +1,12 @@
+import moment from "moment";
+import 'moment/locale/es'
+
+
 const formatDate = (locale, date) => {
-    const options = {
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric',
-    };
-    return new Intl.DateTimeFormat(locale, options).format(new Date(date));
-  };
-  
-  export default formatDate;
+  const format = `${locale === 'es' ? 'D MMM' : 'MMM D,'} YYYY`
+  const jsDate = new Date(date);
+  moment.locale(locale);
+  return moment(jsDate).format(format);
+};
+
+export default formatDate;
