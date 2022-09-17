d1e1d3747d06407c8959df0dbc764e1da3072fd0
unknown <mateolewinzon@gmail.com>
2022-09-17 15:16:18 -0300

    missing key props

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -9,10 +9,10 @@ export default function Footer() {
     <footer className="mt-20 mb-10 mx-auto max-w-2xl items-center">
       <hr className="text-gray-light dark:text-black-800" />
       <div className="grid md:grid-cols-3">
-        {footers.map((footer) => (
-          <div className="flex flex-col ">
+        {footers.map((footer, key) => (
+          <div key={key} className="flex flex-col ">
             {footer.map((item) => (
-              <SpanSecondary className="my-3 text-gray font-semibold">
+              <SpanSecondary key={item.id} className="my-3 text-gray font-semibold">
                 <Link href={item.href}>{i18n.t(`navbar.${item.id}`)}</Link>
               </SpanSecondary>
             ))}
