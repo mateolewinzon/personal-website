cb74abf81351ccc8a73600a506e350038c59350c
unknown <mateolewinzon@gmail.com>
2022-12-18 00:50:20 -0300

    fix:details

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,1 +1,7 @@
-export const fetcher = (...args) => fetch(...args).then(res => res.json())
+export const fetcher = async (
+    input: RequestInfo,
+    init: RequestInit,
+  ) => {
+    const res = await fetch(input, init);
+    return res.json();
+  };
