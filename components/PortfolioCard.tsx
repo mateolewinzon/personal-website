2fc6ea34738689e1e89d3bbe88ad3fa39cbe6e3f
unknown <mateolewinzon@gmail.com>
2023-02-01 11:25:29 -0300

    small fixes

--- components/PortfolioCard.tsx
+++ components/PortfolioCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const PortfolioCard = ({ project }: Props) => (
   <div className="grid sm:grid-cols-3 gap-2 outline outline-1 rounded-md outline-neutral-200 hover:outline-neutral-300 dark:outline-neutral-800 dark:hover:outline-neutral-700 p-4 m-4 md:max-w-2xl rem py-5">
     <div className="flex flex-col sm:col-span-2 gap-3 sm:gap-0 justify-between">
-      <a target="_blank" href={project.url}>
+      <a target="_blank" rel="noopener" href={project.url}>
         <div className="flex items-center gap-2">
           <Span className="text-3xl font-semibold hover:underline">
             {project.title}
@@ -24,11 +24,11 @@ export const PortfolioCard = ({ project }: Props) => (
             viewBox="0 0 16 16"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
             />
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
             />
           </svg>
@@ -37,7 +37,7 @@ export const PortfolioCard = ({ project }: Props) => (
       <Span className="text-base">{project.description}</Span>
       <div className="flex items-center">
         <PostTags tags={project.tags} />
-        <a href={project.repoURL} target="_blank">
+        <a href={project.repoURL} rel="noopener" target="_blank">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="25"
