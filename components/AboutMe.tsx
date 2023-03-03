120ea7346f78b0b8f42f1a558285b080af9498fc
unknown <mateolewinzon@gmail.com>
2023-03-03 18:01:23 -0300

    fix buttons

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -10,47 +10,42 @@ export const AboutMe = () => {
 
   return (
     <section className="bg-home-gradient">
-        <div className="flex max-w-5xl mx-auto min-h-screen items-center p-4 gap-4">
-          <div className="flex flex-col gap-3">
-            <h1>
-              <span className="text-4xl">{i18n.t("about.about")}</span>{" "}
-              <span
-                className={twMerge(
-                  "text-4xl colorful-text",
-                  styles.typewriter
-                )}
-              >
-                {i18n.t("about.me")}
-              </span>
-            </h1>
-            <span className="text-gray-900 dark:text-gray-100 text-xl">
-              {i18n.t("about.about_description")}
+      <div className="flex max-w-5xl mx-auto min-h-screen items-center p-4 gap-4">
+        <div className="flex flex-col gap-3">
+          <h1>
+            <span className="text-4xl">{i18n.t("about.about")}</span>{" "}
+            <span
+              className={twMerge("text-4xl colorful-text", styles.typewriter)}
+            >
+              {i18n.t("about.me")}
             </span>
-            <div className="flex flex-col my-3">
-              <Link href={i18n.t("about.about_skills_url")}>
-                <a className="button">{i18n.t("about.about_skills")}</a>
-              </Link>
-              <Link href={i18n.t("about.about_experience_url")}>
-                <a className="button">
-                  {i18n.t("about.about_experience")}
-                </a>
-              </Link>
-              <Link href="/Mateo_Lewinzon_CV.pdf">
-                <a className="button" target="_blank">
-                  {i18n.t("about.cv")}
-                </a>
-              </Link>
-            </div>
-          </div>
-          <div className="hidden md:inline flex-none">
-            <Image
-              alt="mateo_mountains"
-              height={300}
-              width={300}
-              src="/images/mateo_mountains.jpg"
-            />
+          </h1>
+          <span className="text-gray-900 dark:text-gray-100 text-xl">
+            {i18n.t("about.about_description")}
+          </span>
+          <div className="flex flex-col my-3">
+            <Link href={i18n.t("about.about_skills_url")}>
+              <button className="button">{i18n.t("about.about_skills")}</button>
+            </Link>
+            <Link href={i18n.t("about.about_experience_url")}>
+              <button className="button">
+                {i18n.t("about.about_experience")}
+              </button>
+            </Link>
+            <Link target="_blank" locale="" href="/Mateo_Lewinzon_CV.pdf">
+              <button className="button">{i18n.t("about.cv")}</button>
+            </Link>
           </div>
         </div>
+        <div className="hidden md:inline flex-none">
+          <Image
+            alt="mateo_mountains"
+            height={300}
+            width={300}
+            src="/images/mateo_mountains.jpg"
+          />
+        </div>
+      </div>
     </section>
   );
 };
