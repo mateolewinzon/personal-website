a51849dd5bec4cc248f78173b7f6e663f4e6af3b
Mateo Lewinzon <mateolewinzon@gmail.com>
2024-05-26 13:59:44 -0300

    update meta

--- components/BlogHead.tsx
+++ components/BlogHead.tsx
@@ -1,12 +1,16 @@
-import Head from "next/head"
-import type { Post } from "lib/types"
+import Head from "next/head";
+import type { Post } from "lib/types";
 
-type Props = { data: Post }
+type Props = { data: Post };
 
-export const BlogHead = ({ data }: Props) => <Head>
-  <title>{data.title}</title>
-  <meta name="description" content={data.teaser} />
-  <meta name="og:title" content={data.title} />
-  <meta name="og:description" content={data.teaser} />
-  <meta name="twitter:card" content={data.teaser}></meta>
-</Head>
+export const BlogHead = ({ data }: Props) => (
+  <Head>
+    <title>{data.title}</title>
+    <meta name="description" content={data.teaser} />
+    <meta name="og:title" content={data.title} />
+    <meta name="og:description" content={data.teaser} />
+    <meta name="twitter:card" content="summary"></meta>
+    <meta name="twitter:creator" content="@mateolewinzon" />
+    {data.image && <meta name="og:image" content={data.image} />}
+  </Head>
+);
