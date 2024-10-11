c0e3bfd1c4bb6e517f925adf1d3aa3b9a75bf779
Mateo Lewinzon <mateolewinzon@gmail.com>
2024-10-10 21:23:41 -0300

    fix head ?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,15 @@ import "styles/globals.css";
 
 import { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";
+import { AppHead } from "components/AppHead";
 
 function MyApp({ Component, pageProps }: AppProps<{ lngDict: {} }>) {
   const { lngDict, ...rest } = pageProps;
 
   return (
     <ThemeProvider attribute="class">
+      <AppHead />
       <Component {...rest} />
       <Analytics />
     </ThemeProvider>
--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -8,18 +8,9 @@ import { GET_PROJECTS } from "lib/queries";
 import type { GetStaticProps } from "next";
 
 export default function Portfolio({ projects }) {
-  console.log(projects[0].url);
-
   const { t } = useTranslation();
   return (
     <Container>
-      <Head>
-        <title>Portfolio - Mateo Lewinzon</title>
-        <meta
-          name="description"
-          content="Dale una mirada a mis projectos personales!"
-        />
-      </Head>
         <h1>{t("portfolio.heading")}</h1>
         {projects.map((project, key) => (
           <FadeOnView key={key}>
